66dee91247f776d72aafbcb97d3e89dd3bd2e614
Rompetr78 <rompetr@yahoo.com>
2024-11-05 13:36:47 +0800

    Добавлен комбобокс для выбора тегов. Поле tag_entry удалено

--- Cataas.py
+++ Cataas.py
@@ -1,8 +1,10 @@
 from tkinter import *
+from tkinter import ttk
 from PIL import Image, ImageTk
 import requests
 from io import BytesIO
 
+Allowed_tags = ["sleep", "jump", "fight", "black", "white", "bengal", "siamese", "cute"]
 
 def load_image(url):
     try:
@@ -13,12 +15,12 @@ def load_image(url):
         img.thumbnail((600, 480), Image.Resampling.LANCZOS)
         return ImageTk.PhotoImage(img)
     except Exception as e:
-        print(f"Произошла ошибка {e}")
+        print(f"Произошла ошибка: {e}")
         return None
 
 
 def open_new_window():
-    tag = tag_entry.get()
+    tag = tag_combobox.get()
     url_tag = f"https://cataas.com/cat/{tag}" if tag else "https://cataas.com/cat"
     img = load_image(url_tag)
     if img:
@@ -38,11 +40,6 @@ window = Tk()
 window.title("Cats!")
 window.geometry("600x520")
 
-tag_entry = Entry()
-tag_entry.pack()
-
-load_button = Button(text="Загрузить по тегу", command=open_new_window)
-load_button.pack()
 
 menu_bar = Menu(window)
 window.config(menu=menu_bar)
@@ -54,5 +51,14 @@ file_menu.add_separator()
 file_menu.add_command(label="Выход", command=exit)
 
 url = "https://cataas.com/cat"
+
+tag_label = Label(text="Выбери тег")
+tag_label.pack()
+
+tag_combobox = ttk.Combobox(values=Allowed_tags)
+tag_combobox.pack()
+
+load_button = Button(text="Загрузить по тегу", command=open_new_window)
+load_button.pack()
 
 window.mainloop()
