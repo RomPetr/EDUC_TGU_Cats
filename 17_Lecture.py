afa23a541f3632df59fb82561615a96f09f36e0c
Rompetr78 <rompetr@yahoo.com>
2024-11-06 23:44:42 +0800

    Добавлена кнопка и блок try except в функции 'get_image' в файле 17_Lecture.py

--- 17_Lecture.py
+++ 17_Lecture.py
@@ -2,13 +2,26 @@ import requests
 from tkinter import *
 from PIL import Image, ImageTk
 from io import BytesIO
+from tkinter import messagebox as mb
 
 def get_image(url_api):
-    answer = requests.get(url_api)
-    image = BytesIO(answer.content)
-    img = Image.open(image)
-    img_tk = ImageTk.PhotoImage(img)
-    return img_tk
+    try:
+        answer = requests.get(url_api)
+        image = BytesIO(answer.content)
+        img = Image.open(image)
+        img_tk = ImageTk.PhotoImage(img)
+        return img_tk
+    except Exception as e:
+        mb.showerror("Ошибка", f"Произошла ошибка: {e}")
+        return None
+
+
+def set_image():
+    img = get_image(url)
+
+    if img:
+        label.config(image=img)
+        label.image = img
 
 
 url = "https://cataas.com/cat"
@@ -18,6 +31,10 @@ window =Tk()
 label = Label(window)
 label.pack()
 
-get_image(url)
+btn = Button(window, text="Получить котика", command=set_image)
+btn.pack(pady=10)
+
+
+
 
 window.mainloop()
