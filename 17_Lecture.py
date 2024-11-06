62c776c5622f50584f7bc6a359236d1381b6e869
Rompetr78 <rompetr@yahoo.com>
2024-11-06 23:56:04 +0800

    Добавлено меню в файле 17_Lecture.py

--- 17_Lecture.py
+++ 17_Lecture.py
@@ -9,6 +9,7 @@ def get_image(url_api):
         answer = requests.get(url_api)
         image = BytesIO(answer.content)
         img = Image.open(image)
+        img.thumbnail((550, 500))
         img_tk = ImageTk.PhotoImage(img)
         return img_tk
     except Exception as e:
@@ -24,17 +25,28 @@ def set_image():
         label.image = img
 
 
+def exit_win():
+    window.destroy()
+
+
 url = "https://cataas.com/cat"
 
 window =Tk()
+window.geometry("600x600")
 
 label = Label(window)
 label.pack()
 
-btn = Button(window, text="Получить котика", command=set_image)
-btn.pack(pady=10)
+# btn = Button(window, text="Получить котика", command=set_image)
+# btn.pack(pady=10)
 
+main_menu = Menu(window)
+window.config(menu=main_menu)
+file_menu = Menu(main_menu, tearoff=0)
+main_menu.add_cascade(label="Файл", menu=file_menu)
 
-
+file_menu.add_command(label="Получить котика", command=set_image)
+file_menu.add_separator()
+file_menu.add_command(label="Выход", command=exit_win)
 
 window.mainloop()
